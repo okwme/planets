36400ff994edf7c8164d0193f6775fc1101f6e5e
Billy Rennekamp <billy.rennekamp@gmail.com>
2019-03-03 21:03:37 +0100

    kinda

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
 <style lang="stylus">
+html, body
+  padding 0
+  margin 0
 #app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
+  font-family Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align left
-  color #2c3e50
+  text-align center
+  color black
+  margin-top 10px
 
 #nav
   padding 30px
   a
     font-weight bold
-    color #2c3e50
+    color black
     &.router-link-exact-active
-      color #42b983
+      color blue
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,87 @@
 <template lang="pug">
   .home
-    input(v-model="offset")
-    br
-    span(v-for="(v, n) in names" :key="v.name" v-html="v.svg") 
-    br
-    button(@click="addMore") More
+    .pic
+      .bigboy(:style="`background-image: url(data:image/svg+xml;base64,${svg});`")
+      //- .bigboy(v-html="svg")
+      .NW.dir(@click="clickDir('NW')")
+      .NE.dir(@click="clickDir('NE')")
+      .SW.dir(@click="clickDir('SW')")
+      .SE.dir(@click="clickDir('SE')")
+    table
+      tr
+        th(colspan="2") {{(prettyName)}}
+      tr
+        th NW
+        th NE
+      tr
+        td 
+          input(type="number" v-model="NW")
+        td
+          input(type="number" v-model="NE")
+      tr
+        th SW
+        th SE
+      tr
+        td
+          input(type="number" v-model="SW")
+        td
+          input(type="number" v-model="SE")
+    button(@click="randomize") RaNdOm
 </template>
 
 <script>
+
 import { pour, PlainSVGStringRenderer } from 'sigil-js'
 import dirs from '../assets/dirs.json'
 export default {
   name: 'home',
   data() {
     return {
-      offset: 0,
-      names: [],
-      additional: 10
+      NW: rand(dirs.NW.length),
+      NE: rand(dirs.NE.length),
+      SW: rand(dirs.SW.length),
+      SE: rand(dirs.SE.length)
+    }
+  },
+  computed: {
+    prettyName() {
+      return '~' + JSON.parse(JSON.stringify(this.name)).splice(6, 0, '-')
+    },
+    name () {
+      return this.getNameFromDirs({NW: this.NW, NE: this.NE, SW: this.SW, SE: this.SE})
+    },
+    svg () {
+      const r = pour({
+        patp: this.name,
+        renderer: undefined,
+        size: 128
+      });
+      return btoa(PlainSVGStringRenderer.svg(r))//.replace(/\r/g, "").replace(/\n/g, "");
+
     }
   },
   methods: {
-    addMore () {
-      for (let i = (this.offset * this.additional); i < ((this.offset + 1) * this.additional); i++) {
-        let name = this.getNameFromN(i)
-        const r = pour({
-          patp: name,
-          renderer: undefined,
-          size: 128
-        });
-        const data = PlainSVGStringRenderer.svg(r)
-        this.names.push({
-          name,
-          svg: data
-        })
+    randomize () {
+      this.NW = rand(dirs.NW.length)
+      this.NE = rand(dirs.NE.length)
+      this.SW = rand(dirs.SW.length)
+      this.SE = rand(dirs.SE.length)
+    },
+    clickDir (dir) {
+      switch (dir){
+        case 'NW':
+          this.NW = (this.NW + 1) % dirs.NW.length
+        break;
+        case 'NE':
+          this.NE = (this.NE + 1) % dirs.NE.length
+        break;
+        case 'SW':
+          this.SW = (this.SW + 1) % dirs.SW.length
+        break;
+        case 'SE':
+          this.SE = (this.SE + 1) % dirs.SE.length
+        break;
       }
-      this.offset += 1
     },
     getNameFromDirs({NW, NE, SW, SE}) {
       return dirs.NW[NW] +
@@ -54,4 +101,66 @@ export default {
     }
   }
 }
+function rand(max) {
+  return Math.floor(Math.random() * max);
+}
+if (!String.prototype.splice) {
+    /**
+     * {JSDoc}
+     *
+     * The splice() method changes the content of a string by removing a range of
+     * characters and/or adding new characters.
+     *
+     * @this {String}
+     * @param {number} start Index at which to start changing the string.
+     * @param {number} delCount An integer indicating the number of old chars to remove.
+     * @param {string} newSubStr The String that is spliced in.
+     * @return {string} A new string with the spliced substring.
+     */
+    String.prototype.splice = function(start, delCount, newSubStr) {
+        return this.slice(0, start) + newSubStr + this.slice(start + Math.abs(delCount));
+    };
+}
 </script>
+<style lang="stylus">
+  table
+    margin 10px auto 20px auto
+    th
+      padding-top 20px
+    td
+      padding 0px 20px
+  .pic
+    position relative
+    // border 1px solid blue
+    .bigboy
+      width 60vh
+      height 60vh
+      margin auto
+      background-position center center
+      background-repeat no-repeat
+      background-size contain
+  .dir
+    position absolute
+    // border 1px solid red
+    cursor: pointer
+  .NW
+    top 0
+    left 0
+    right 50%
+    bottom 50%
+  .NE
+    top 0
+    right 0
+    left 50%
+    bottom 50%
+  .SE
+    top 50%
+    bottom 0
+    right 0
+    left 50%
+  .SW
+    top 50%
+    bottom 0
+    left 0
+    right 50%
+</style>
