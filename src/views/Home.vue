83379ec33109a2fafe6a6151329e74191e3e4208
Billy Rennekamp <billy.rennekamp@gmail.com>
2019-03-03 16:38:44 +0100

    list them from 0

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  .home
+    input(v-model="offset")
+    br
+    span(v-for="(v, n) in names" :key="v.name" v-html="v.svg") 
+    br
+    button(@click="addMore") More
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { pour, PlainSVGStringRenderer } from 'sigil-js'
+import dirs from '../assets/dirs.json'
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      offset: 0,
+      names: [],
+      additional: 10
+    }
+  },
+  methods: {
+    addMore () {
+      for (let i = (this.offset * this.additional); i < ((this.offset + 1) * this.additional); i++) {
+        let name = this.getNameFromN(i)
+        const r = pour({
+          patp: name,
+          renderer: undefined,
+          size: 128
+        });
+        const data = PlainSVGStringRenderer.svg(r)
+        this.names.push({
+          name,
+          svg: data
+        })
+      }
+      this.offset += 1
+    },
+    getNameFromDirs({NW, NE, SW, SE}) {
+      return dirs.NW[NW] +
+        dirs.NE[NE] +
+        dirs.SW[SW] +
+        dirs.SE[SE]
+    },
+    getNameFromN (n) {
+      let NW = n % dirs.NW.length
+      n =  Math.floor(n / dirs.NW.length)
+      let NE = n % dirs.NE.length
+      n =  Math.floor(n / dirs.NE.length)
+      let SW = n % dirs.SW.length
+      n =  Math.floor(n / dirs.SW.length)
+      let SE = n % dirs.SE.length
+      return this.getNameFromDirs({NW, NE, SW, SE})
+    }
   }
 }
 </script>
